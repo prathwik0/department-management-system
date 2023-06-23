e7f66b53a605c510a5bb64b4e818b980acd6dfd0
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-23 14:34:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
 
     <?php include("header.php"); ?>
 
-    <a href="student.php"> student<br>
-        <a href="department.php"> department<br>
-            <a href="course.php"> course<br>
-                <a href="project.php"> project<br>
-                    <a href="marks.php"> marks<br>
-                        <a href="internship.php"> department<br>
-                            <a href="faculty.php"> faculty<br>
-                                <a href="mooc_course.php"> mooc_course<br>
-                                    <a href="teacher_activities.php"> teacher_activities<br>
+    <a href="./student.php"> student</a><br>
+    <a href="./department.php"> department</a><br>
+    <a href="./course.php"> course</a><br>
+    <a href="./project.php"> project</a><br>
+    <a href="./marks.php"> marks</a><br>
+    <a href="./internship.php"> department</a><br>
+    <a href="./faculty.php"> faculty</a><br>
+    <a href="./mooc_course.php"> mooc_course</a><br>
+    <a href="./teacher_activities.php"> teacher_activities</a><br>
 </body>
 
 </html>
