18d5ecbd67eb7a07478ee9c78408a7663f9cd6c7
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-23 16:16:46 +0530

    Update department.php

--- department.php
+++ department.php
@@ -1,53 +1,55 @@
 <?php
-    
-    include('dbCon.php');
-    
-    if (isset($_POST['submit'])) {
-        $name=$_POST['name'];
-        $dep_id=$_POST['dep_id'];
-        $office=$_POST['office']
-        $hod=$_POST['hod'];
-        
-        $sqlInsertIntoDB = "INSERT INTO `department`(`Name`, `Department_ID`, `Office`, `hod_id`) VALUES ('$name','$dep_id','$office,'$hod')";
-        if (mysqli_query($conn, $sqlInsertIntoDB)) {
-            Print '<script>alert("Details uploaded successfully.");</script>';
-            Print '<script>window.location.assign("index.php");</script>';
-        } else {
-            echo "<br />Failed to upload.<br />";
-        }
+
+include('dbCon.php');
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $dep_id = $_POST['dep_id'];
+    $office = $_POST['office'];
+    $hod = $_POST['hod'];
+
+    $sqlInsertIntoDB = "INSERT INTO `department`(`Name`, `Department_ID`, `Office`, `hod_id`) VALUES ('$name','$dep_id','$office,'$hod')";
+    if (mysqli_query($conn, $sqlInsertIntoDB)) {
+        print '<script>alert("Details uploaded successfully.");</script>';
+        print '<script>window.location.assign("index.php");</script>';
     } else {
-        
+        echo "<br />Failed to upload.<br />";
     }
-    
-    //Close the connection
-    if (mysqli_close($conn)) {
-        //echo "<br />Connection Closed.";
-    }
-    ?>
+} else {
+}
+
+//Close the connection
+if (mysqli_close($conn)) {
+    //echo "<br />Connection Closed.";
+}
+?>
 
 <!DOCTYPE html>
 <html>
-<head>
-<title>Department</title>
-</head>
-<body>
-<h3>Insert Department Details:</h3>
-<form action="" method="post" enctype="multipart/form-data">
-Department Name:<br>
-<input type="text" name="name">
-<br>
-Department ID:<br>
-<input type="text" name="dep_id" maxlength="30">
-<br>
-Office:<br>
-<input type="text" name="office" maxlength="255">
-<br>
-Head of Department's ID:<br>
-<input type="text" name="hod">
-<br>
-<br />
-<input type="submit" name="submit" value="Upload">
 
-</form>
+<head>
+    <title>Department</title>
+</head>
+
+<body>
+    <h3>Insert Department Details:</h3>
+    <form action="" method="post" enctype="multipart/form-data">
+        Department Name:<br>
+        <input type="text" name="name">
+        <br>
+        Department ID:<br>
+        <input type="text" name="dep_id" maxlength="30">
+        <br>
+        Office:<br>
+        <input type="text" name="office" maxlength="255">
+        <br>
+        Head of Department's ID:<br>
+        <input type="text" name="hod">
+        <br>
+        <br />
+        <input type="submit" name="submit" value="Upload">
+
+    </form>
 </body>
+
 </html>
