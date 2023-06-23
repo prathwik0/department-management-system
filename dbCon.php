c9e9f8a3d9f2e507fdc3f88934b13823382b93bc
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-23 16:16:31 +0530

    Update dbCon.php

--- dbCon.php
+++ dbCon.php
@@ -4,16 +4,19 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$conn = mysqli_connect($servername, $username, $password);
-if ($conn) {
-    //echo "Connected to server successfully";
+$db_name = "aiml";
+
+$conn = mysqli_connect($servername, $username, $password, $db_name);
+if ($conn->connect_error) {
+    echo '<script>alert("Connection failed: ' . $conn->connect_error . '");</script>';
 } else {
-    die("Failed To Connect to server " . mysqli_connect_error());
+    echo "<script>alert('Connection with database successful')</script>";
 }
 
-$selectalreadycreateddatabase = mysqli_select_db($conn, "aiml");
-if ($selectalreadycreateddatabase) {
-    //echo "<br /> Existing database selected successfully";
-} else {
-    echo "<br /> Selected Database Not Found";
-}
+// $selectalreadycreateddatabase = mysqli_select_db($conn, $db_name);
+// if ($selectalreadycreateddatabase) {
+//     echo "<script>alert('Selected ", $db_name, " database!')</script>";
+//     //echo "<br /> Existing database selected successfully";
+// } else {
+//     echo "<script>alert('Selected Database Not Found')</script>";
+// }
